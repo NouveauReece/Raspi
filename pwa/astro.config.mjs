f7971f65ee7bfde7fd61cceb6932a5396eeb6a41
NouveauReece <nouveaureece@protonmail.com>
2026-04-08 17:51:09 -0400

    temp disable pwa

--- pwa/astro.config.mjs
+++ pwa/astro.config.mjs
@@ -4,23 +4,23 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [
-      VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Raspi App',
-        short_name: 'Raspi',
-        start_url: '/',
-        display: 'standalone',
-        theme_color: '#000000',
-        icons: [
-          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' }
-        ]
-      },
-      workbox: { navigateFallback: '/404' }
-    })
-  ]
-  }
+  // vite: {
+  //   plugins: [
+  //     VitePWA({
+  //     registerType: 'autoUpdate',
+  //     manifest: {
+  //       name: 'Raspi App',
+  //       short_name: 'Raspi',
+  //       start_url: '/',
+  //       display: 'standalone',
+  //       theme_color: '#000000',
+  //       icons: [
+  //         { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
+  //         { src: '/icon-512.png', sizes: '512x512', type: 'image/png' }
+  //       ]
+  //     },
+  //     workbox: { navigateFallback: '/404' }
+  //   })
+  // ]
+  // }
 })
